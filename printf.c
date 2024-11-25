680667a82b25060064b86645ace34c548bd3caa0
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-11-25 10:26:45 +0100

    add function to detect format

--- printf.c
+++ printf.c
@@ -1,6 +1,24 @@
 #include "printf.h"
 
 /**
+ * detect_format - function to detect the format after the %
+ * @str: list of types of arguments passed to the function
+ * @detect: structure used to set our index in the string
+ *
+ * Return: the character after the %
+ */
+char detect_format(const char *str, detect_t *detect)
+{
+	for (; str[detect->index] != '%'; detect->index += 1)
+	{
+	}
+	printf("return = %c\n index at = %i", str[detect->index + 1],
+		detect->index);
+	return (str[detect->index + 1]);
+}
+
+
+/**
  * _printf - prints anything based on a format string
  * @format: list of types of arguments passed to the function
  *
@@ -8,5 +26,17 @@
  */
 int _printf(const char *format, ...)
 {
-    return (0);
+	va_list arg;
+	detect_t *detect;
+
+	int (*flag[2])(va_list, void*);
+	flag[0] = print_character;
+	detect = malloc(sizeof(detect_t));
+	detect->index = 0;
+	if (format == NULL)
+		return (0);
+	detect_format(format, detect);
+	printf("index value = %i", detect->index);
+	free(detect);
+	return (0);
 }
