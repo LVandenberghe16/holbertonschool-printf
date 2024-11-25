c2e8b20d92299a685b1c252a7a2454e3d8c9b904
Braga <ibrahim.houmaidi@gmail.com>
2024-11-25 15:49:51 +0100

    add new flags

--- character_flag.c
+++ character_flag.c
@@ -1,4 +1,4 @@
-#include "printf.h"
+#include "main.h"
 
 /**
  * print_character - Imprime un seul caractère
@@ -27,3 +27,29 @@ void print_string(const char *str, print_t *data)
     data->size += _strlen(str);
     _putstr(str);
 }
+
+/**
+ * print_character - Imprime un seul caractère
+ * @d: caractère à imprimer
+ * @data: structure contenant la taille de la chaîne générée
+ */
+void print_double(int d, print_t *data)
+{
+	if (data == NULL)
+		return;
+
+	data->size += print_big_numbers(d);
+}
+
+/**
+ * print_character - Imprime un seul caractère
+ * @i: caractère à imprimer
+ * @data: structure contenant la taille de la chaîne générée
+ */
+void print_integer(int i, print_t *data)
+{
+	if (data == NULL)
+		return;
+
+	data->size += print_numbers(i);
+}	
