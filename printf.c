bf788ec2aee206e7f1b85c62a55f24565da27a19
Braga <ibrahim.houmaidi@gmail.com>
2024-11-25 14:34:05 +0100

    fix return value

--- printf.c
+++ printf.c
@@ -1,29 +1,6 @@
 #include "printf.h"
 
 /**
- * detect_format - Détecte le format après un caractère %
- * @str: chaîne formatée
- * @data: structure utilisée pour suivre l'état de l'analyse
- *
- * Return: le caractère après %
- */
-char detect_format(const char *str, print_t *data)
-{
-	if (str == NULL || data == NULL)
-		return ('\0');
-
-	for (; str[data->index] != '%' && str[data->index] != '\0'; data->index += 1)
-	{
-		data->size += 1;
-	}
-
-	if (str[data->index] == '\0')
-		return ('\0'); /* Retourne '\0' si le format est incomplet */
-
-	return (str[data->index + 1]);
-}
-
-/**
  * init_data - Initialise la structure print_t
  * @data: structure à initialiser
  *
@@ -31,17 +8,16 @@ char detect_format(const char *str, print_t *data)
  */
 print_t *init_data(print_t *data)
 {
-	if (data == NULL)
-		return (NULL);
+    if (data == NULL)
+        return (NULL);
 
-	data->index = 0;
-	data->size = 0;
+    data->size = 0;
 
-	/* Initialise les handlers */
-	data->handlers[0] = print_character;
-	data->handlers[1] = print_string;
+    /* Associez directement les gestionnaires */
+    data->char_handler = print_character;
+    data->string_handler = print_string;
 
-	return (data);
+    return (data);
 }
 
 /**
@@ -52,38 +28,41 @@ print_t *init_data(print_t *data)
  */
 int _printf(const char *format, ...)
 {
-	print_t *data;
-	va_list args;
-	int size;
-	int i;
+    print_t *data;
+    va_list args;
+    int size;
+    int i;
 
-	if (!format)
-		return (-1);
-	data = malloc(sizeof(print_t));
-	if (!data)
-		return (-1);
-	data->size = 0;
-	va_start(args, format);
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%' && format[i + 1])
-		{
-			i++;
-			if (format[i] == 'c')
-				print_character(va_arg(args, int), data);
-			else if (format[i] == 's')
-				print_string(va_arg(args, char *), data);
-			else
-				write(1, &format[i], 1), data->size++;
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			data->size++;
-		}
-	}
-	va_end(args);
-	size = data->size;
-	free(data);
-	return (size);
+    if (!format)
+        return (-1);
+    data = malloc(sizeof(print_t));
+    if (!data)
+        return (-1);
+    init_data(data);
+    va_start(args, format);
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%' && format[i + 1])
+        {
+            i++;
+            if (format[i] == 'c')
+                data->char_handler(va_arg(args, int), data);
+            else if (format[i] == 's')
+                data->string_handler(va_arg(args, char *), data);
+            else
+            {
+                write(1, &format[i - 1], 1);
+                data->size++;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            data->size++;
+        }
+    }
+    va_end(args);
+    size = data->size;
+    free(data);
+    return (size);
 }
