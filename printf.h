c7a88b44b4ebc5f6d815f6c72fd36987a2a5ebb6
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-11-25 10:26:28 +0100

    add a structure and prototypes

--- printf.h
+++ printf.h
@@ -3,7 +3,19 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct detect - a structure representing a point in 2D space
+ * @index: used to define where we are when we read a string
+ */
+typedef struct detect
+{
+	int index;
+} detect_t;
 
 int _printf(const char *format, ...);
+void print_character(const char c);
 
 #endif /*PRINTF_H*/
